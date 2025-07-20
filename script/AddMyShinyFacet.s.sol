297e9e2f7e0dd4c310b6c21baf95a4811a32f197
David Dada <daveproxy80@gmail.com>
2025-07-20 20:30:27 +0100

    refactor: Update AddMyShinyFacet contract to use AddMyShinyFacetHelper for facet deployment

--- script/AddMyShinyFacet.s.sol
+++ script/AddMyShinyFacet.s.sol
@@ -2,33 +2,40 @@
 pragma solidity ^0.8.4;
 
 import {Script} from "forge-std/Script.sol";
-import {DeployedDiamondState} from "@diamond-test/helpers/TestStates.sol";
+import {Diamond} from "@diamond/Diamond.sol";
+import {HelperContract} from "@diamond-test/helpers/HelperContract.sol";
+import {AddMyShinyFacetHelper} from "@diamond-test/helpers/AddMyShinyFacetHelper.sol";
 import {MyShinyFacet} from "@diamond/facets/MyShinyFacet.sol";
 import {FacetCutAction, FacetCut} from "@diamond/libraries/types/DiamondTypes.sol";
 import {IDiamondCut} from "@diamond/interfaces/IDiamondCut.sol";
 
-contract AddMyShinyFacet is Script, DeployedDiamondState {
-    MyShinyFacet myShinyFacet;
-
-    function run() external {
+contract AddMyShinyFacet is Script, AddMyShinyFacetHelper {
+    function run(Diamond diamond) external returns (MyShinyFacet myShinyFacet_) {
         vm.startBroadcast();
 
-        // Deploy MyShinyFacet contract
-        myShinyFacet = new MyShinyFacet();
-
-        // Create the array for FacetCuts
-        FacetCut[] memory cut = new FacetCut[](1);
-
-        // Add MyShinyFacet address to the array with the action and generate it's selectors
-        cut[0] = FacetCut({
-            facetAddress: address(myShinyFacet),
-            action: FacetCutAction.Add,
-            functionSelectors: _generateSelectors("MyShinyFacet")
-        });
-
-        // Call diamondCut on the diamond address
-        IDiamondCut(address(diamond)).diamondCut(cut, address(0), "");
+        myShinyFacet_ = _addMyShinyFacet(diamond);
 
         vm.stopBroadcast();
+    }
+}
+
+contract SetAndGetShine is Script {
+    MyShinyFacet myShinyFacet;
+
+    function run(address diamond) public returns (string memory, uint8) {
+        // Deploy MyShinyFacet contract
+        vm.startBroadcast();
+
+        // Call diamondCut on the diamond address
+        bytes memory call = abi.encodeWithSelector(MyShinyFacet.setShine.selector, 50);
+        (bool success,) = diamond.call(call);
+        if (!success) revert("");
+
+        bytes memory call1 = abi.encodeWithSelector(MyShinyFacet.getShine.selector);
+        (bool success1, bytes memory ret) = diamond.call(call1);
+        if (!success1) revert("");
+
+        vm.stopBroadcast();
+        return (string(ret), uint8(ret.length));
     }
 }
