1b19c786387b685c6f26eaa9d32d3c0b263d0ba5
David Dada <daveproxy80@gmail.com>
2025-07-21 10:02:16 +0100

    refactor: update DeployedDiamondState to use facet interfaces directly

--- test/helpers/TestStates.sol
+++ test/helpers/TestStates.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.4;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployDiamond} from "@diamond-script/DeployDiamond.s.sol";
 import {DeployDiamondHelper} from "@diamond-test/helpers/DeployDiamondHelper.sol";
 import {Diamond} from "@diamond/Diamond.sol";
-import {IDiamondCut} from "@diamond/interfaces/IDiamondCut.sol";
-import {IDiamondLoupe} from "@diamond/interfaces/IDiamondLoupe.sol";
+import {DiamondCutFacet} from "@diamond/facets/DiamondCutFacet.sol";
+import {DiamondLoupeFacet} from "@diamond/facets/DiamondLoupeFacet.sol";
+import {OwnableRolesFacet} from "@diamond/facets/OwnableRolesFacet.sol";
 
 /// @notice Provides shared state for tests involving a freshly deployed Diamond contract.
 /// @dev Sets up references to deployed facets, interfaces, and the diamond itself for testing.
@@ -15,10 +15,13 @@ abstract contract DeployedDiamondState is DeployDiamondHelper {
     Diamond public diamond;
 
     /// @notice Interface for the DiamondCut functionality of the deployed diamond.
-    IDiamondCut public diamondCut;
+    DiamondCutFacet public diamondCut;
 
     /// @notice Interface for the DiamondLoupe functionality of the deployed diamond.
-    IDiamondLoupe public diamondLoupe;
+    DiamondLoupeFacet public diamondLoupe;
+
+    /// @notice Interface for the OwnableRoles functionality of the deployed diamond.
+    OwnableRolesFacet public ownableRoles;
 
     /// @notice Stores the facet addresses returned from the diamond loupe.
     address[] public facetAddresses;
@@ -26,13 +29,17 @@ abstract contract DeployedDiamondState is DeployDiamondHelper {
     /// @notice List of facet contract names used in deployment.
     string[3] public facetNames = ["DiamondCutFacet", "DiamondLoupeFacet", "OwnableRolesFacet"];
 
+    address public diamondOwner = makeAddr("Owner");
+
     /// @notice Deploys the Diamond contract and initializes interface references and facet addresses.
     /// @dev This function is intended to be called in a test setup phase (e.g., `setUp()` in Foundry).
     function setUp() public {
-        diamond = _deployDiamond(diamondOwner);
+        vm.startPrank(diamondOwner);
+        diamond = Diamond(_deployDiamond(diamondOwner));
 
-        diamondCut = IDiamondCut(address(diamond));
-        diamondLoupe = IDiamondLoupe(address(diamond));
+        diamondCut = DiamondCutFacet(address(diamond));
+        diamondLoupe = DiamondLoupeFacet(address(diamond));
+        ownableRoles = OwnableRolesFacet(address(diamond));
 
         facetAddresses = diamondLoupe.facetAddresses();
     }
