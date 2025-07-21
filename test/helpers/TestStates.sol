34ac40d1d763321a6aad8740effd57d9db1a641e
David Dada <daveproxy80@gmail.com>
2025-07-21 10:10:43 +0100

    refactor: change diamond variable type from Diamond instance to address

--- test/helpers/TestStates.sol
+++ test/helpers/TestStates.sol
@@ -12,7 +12,7 @@ import {OwnableRolesFacet} from "@diamond/facets/OwnableRolesFacet.sol";
 /// @dev Sets up references to deployed facets, interfaces, and the diamond itself for testing.
 abstract contract DeployedDiamondState is DeployDiamondHelper {
     /// @notice Instance of the deployed Diamond contract.
-    Diamond public diamond;
+    address public diamond;
 
     /// @notice Interface for the DiamondCut functionality of the deployed diamond.
     DiamondCutFacet public diamondCut;
@@ -35,7 +35,7 @@ abstract contract DeployedDiamondState is DeployDiamondHelper {
     /// @dev This function is intended to be called in a test setup phase (e.g., `setUp()` in Foundry).
     function setUp() public {
         vm.startPrank(diamondOwner);
-        diamond = Diamond(_deployDiamond(diamondOwner));
+        diamond = _deployDiamond(diamondOwner);
 
         diamondCut = DiamondCutFacet(address(diamond));
         diamondLoupe = DiamondLoupeFacet(address(diamond));
