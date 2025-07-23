c931928fdb3f8713111c4016f040c5e8f18163a0
David Dada <daveproxy80@gmail.com>
2025-07-23 15:14:33 +0100

    chore: refactor to _diamondStorage

--- src/initializers/ERC165Init.sol
+++ src/initializers/ERC165Init.sol
@@ -13,7 +13,7 @@ contract ERC165Init {
     /// @dev This function is called during the diamond cut process to set up
     ///      the initial state of the contract.
     function initERC165() external {
-        DiamondStorage storage ds = LibDiamond.diamondStorage();
+        DiamondStorage storage ds = LibDiamond._diamondStorage();
         /// @dev type(ERC165).interfaceId
         ds.supportedInterfaces[0x01ffc9a7] = true;
         /// @dev type(IERC173).interfaceId
