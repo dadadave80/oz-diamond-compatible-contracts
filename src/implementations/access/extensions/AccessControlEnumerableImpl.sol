cb7c438fb48cdfa08054902157e9d6266f470904
David Dada <daveproxy80@gmail.com>
2025-09-23 18:08:06 +0100

    chore: remove commented out layout storage

--- src/implementations/access/extensions/AccessControlEnumerableImpl.sol
+++ src/implementations/access/extensions/AccessControlEnumerableImpl.sol
@@ -17,7 +17,6 @@ import {AccessControlEnumerableUpgradeable} from "@diamond/access/extensions/Acc
 /// @custom:storage-location erc7201:openzeppelin.storage.AccessControlEnumerable
 /// @dev keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.AccessControlEnumerable")) - 1)) & ~bytes32(uint256(0xff))
 contract AccessControlEnumerableImpl is AccessControlEnumerableUpgradeable {
-    //layout at 0xc1f6fe24621ce81ec5827caf0253cadb74709b061630e6b55e82371705932000 {
     constructor() {
         _disableInitializers();
     }
