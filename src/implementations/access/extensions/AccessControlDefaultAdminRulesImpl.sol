be851de868c2673173c61547a46159d4699812e7
David Dada <daveproxy80@gmail.com>
2025-09-23 18:08:06 +0100

    chore: remove commented out layout storage

--- src/implementations/access/extensions/AccessControlDefaultAdminRulesImpl.sol
+++ src/implementations/access/extensions/AccessControlDefaultAdminRulesImpl.sol
@@ -18,7 +18,6 @@ import {AccessControlDefaultAdminRulesUpgradeable} from
 /// @custom:storage-location erc7201:openzeppelin.storage.AccessControlDefaultAdminRules
 /// @dev keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.AccessControlDefaultAdminRules")) - 1)) & ~bytes32(uint256(0xff))
 contract AccessControlDefaultAdminRulesImpl is AccessControlDefaultAdminRulesUpgradeable {
-    //layout at 0xeef3dac4538c82c8ace4063ab0acd2d15cdb5883aa1dff7c2673abb3d8698400 {
     constructor() {
         _disableInitializers();
     }
