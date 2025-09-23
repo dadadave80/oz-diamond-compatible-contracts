c2599b891e9bd0d7e5d10a0b06beffe63e66daf0
David Dada <daveproxy80@gmail.com>
2025-09-23 18:41:16 +0100

    chore: move structs and storage representations to their libraries

--- src/access/libraries/LibAccessManaged.sol
+++ src/access/libraries/LibAccessManaged.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {
-    ACCESS_MANAGED_STORAGE_SLOT,
-    AccessManagedStorage
-} from "@diamond/access/libraries/storage/AccessManagedStorage.sol";
-
 import {AuthorityUtils} from "@openzeppelin/contracts/access/manager/AuthorityUtils.sol";
 import {IAccessManaged} from "@openzeppelin/contracts/access/manager/IAccessManaged.sol";
 import {IAccessManager} from "@openzeppelin/contracts/access/manager/IAccessManager.sol";
+
+// keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.AccessManaged")) - 1)) & ~bytes32(uint256(0xff))
+bytes32 constant ACCESS_MANAGED_STORAGE_SLOT = 0xf3177357ab46d8af007ab3fdb9af81da189e1068fefdc0073dca88a2cab40a00;
+
+/// @custom:storage-location erc7201:openzeppelin.storage.AccessManaged
+struct AccessManagedStorage {
+    address authority;
+    bool consumingSchedule;
+}
 
 library LibAccessManaged {
     function _accessManagedStorage() internal pure returns (AccessManagedStorage storage ams_) {
